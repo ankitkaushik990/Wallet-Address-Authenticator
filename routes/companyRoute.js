c39e5c0e3436cb6ac7d6e460006384dc46764c76
Ankit Kaushik <ankit@oodles0>
2023-10-16 18:11:34 +0530

    multer config added

--- routes/companyRoute.js
+++ routes/companyRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
+const upload = require("../config/multer.config");
+
 const { createCompany } = require("../controller/companyController");
 const { isAuthenticated } = require("../config/passport.config");
 
-router.post("/addcompany",isAuthenticated, createCompany);
+router.post("/addcompany", isAuthenticated, createCompany);
 
 module.exports = router;
